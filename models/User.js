4c2a3dfd90991c63af673b22a03b57dfee9c834c
unknown <a.shataie2023@gmail.com>
2025-07-26 12:09:18 -0700

    feat: Create User Model

--- models/User.js
+++ models/User.js
@@ -7,3 +7,6 @@ const UserSchema = new Schema({
     phoneNumber: { type: Number, required: true },
     password: { type: String, required: true },
 }, { timestamps: true });
+
+
+module.exports = mongoose.model("User", UserSchema);
