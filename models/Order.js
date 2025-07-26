e74f7f960dba81bc9cf71cfb1b69e1c6949ac7f6
unknown <a.shataie2023@gmail.com>
2025-07-26 12:13:13 -0700

    feat: Create order schema

---
+++ models/Order.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    food: { type: mongoose.Types.ObjectId, ref: "Food", required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true }
+}, { timestamps: true });
