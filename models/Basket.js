1ca3b7e670f75188864fe6db2e0b37ea9461fb8a
unknown <a.shataie2023@gmail.com>
2025-07-26 12:34:19 -0700

    feat: Create basket model

--- models/Basket.js
+++ models/Basket.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Basket = new Schema({
+const BasketSchema = new Schema({
     user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
     food: { type: mongoose.Types.ObjectId, ref: "Food", required: true },
-    quantity: { type: Number, required: true }    
+    quantity: { type: Number, required: true }
 }, { timestamps: true });
+
+module.exports = mongoose.model("Basket", BasketSchema);
