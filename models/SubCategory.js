ce1fa82812f694487f46667fe043795c293923f8
unknown <a.shataie2023@gmail.com>
2025-07-26 12:22:44 -0700

    feat: Create SubCategory schema

---
+++ models/SubCategory.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SubCategorySchema = new Schema({
+    category: { type: mongoose.Types.ObjectId, ref: "Category", required: true },
+    title: { type: String, required: true },
+    icon: { type: String, required: true }
+}, { timestamps: true });
