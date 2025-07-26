3b753d926cc2d0a28475456dee6c172793503b8e
unknown <a.shataie2023@gmail.com>
2025-07-26 12:36:58 -0700

    feat: Create like model

--- models/Like.js
+++ models/Like.js
@@ -5,3 +5,5 @@ const LikeSchema = new Schema({
     user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
     food: { type: mongoose.Types.ObjectId, ref: "Food", required: true }
 }, { timestamps: true });
+
+module.exports = mongoose.model("Like", LikeSchema);
