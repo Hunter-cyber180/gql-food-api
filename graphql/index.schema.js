2a1f08acc334779a730ad1c530957ed41e2807f3
unknown <a.shataie2023@gmail.com>
2025-07-29 22:02:59 -0700

    feat: Add food types to schema

--- graphql/index.schema.js
+++ graphql/index.schema.js
@@ -1,4 +1,15 @@
+const foodTypes = require("./types/food.types");
+const categoryTypes = require("./types/category.types");
+const userTypes = require("./types/user.types");
+const basketTypes = require("./types/basket.types");
+const likeTypes = require("./types/like.types");
+const subCategoryTypes = require("./types/subcategory.types");
+const orderTypes = require("./types/order.types");
+
 module.exports = `#graphql
+    ${foodTypes.Food}
+    
+
     type Query {
 
     }
