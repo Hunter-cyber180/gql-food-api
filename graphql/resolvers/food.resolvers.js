26b96465b9e2cd800ab0de75954e6498ee4bfbb5
unknown <a.shataie2023@gmail.com>
2025-08-01 23:03:42 -0700

    feat: Create food resolver

--- graphql/resolvers/food.resolvers.js
+++ graphql/resolvers/food.resolvers.js
@@ -5,6 +5,11 @@ const foods = async () => {
     return await FoodModel.find({});
 }
 
+const food = async ({ id: _id }) => {
+    return await FoodModel.findOne({ _id });
+}
+
 module.exports = {
-    foods
+    foods,
+    food
 }
