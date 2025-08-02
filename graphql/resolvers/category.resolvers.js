a0e4042bdde623fc2a72d78672fc265968512a9b
unknown <a.shataie2023@gmail.com>
2025-08-01 23:13:18 -0700

    feat: Create delete category resolver

--- graphql/resolvers/category.resolvers.js
+++ graphql/resolvers/category.resolvers.js
@@ -3,8 +3,11 @@ const CategoryModel = require("../../models/Category");
 
 categories = async () => await CategoryModel.find({});
 
-
+const deleteCategory = async ({ id: _id }) => {
+    return await CategoryModel.findOneAndDelete({ _id });
+}
 
 module.exports = {
-    categories
+    categories,
+    deleteCategory
 }
