b95cfa7cb2ff2bd5ae50497a4ed1147a9fcd5dd7
unknown <a.shataie2023@gmail.com>
2025-08-02 23:00:45 -0700

    feat: Create add subCategory resolver

--- graphql/resolvers/subcategory.resolvers.js
+++ graphql/resolvers/subcategory.resolvers.js
@@ -3,6 +3,13 @@ const SubCategoryModel = require("../../models/SubCategory");
 
 subCategories = async () => await SubCategoryModel.find({});
 
+const addCategory = async (_, args, context) => {
+    const { title, icon, category } = args;
+    // TODO => validate admin token
+    return await SubCategoryModel.create({ title, icon, category });
+}
+
 module.exports = {
     subCategories,
+    addCategory,
 };
