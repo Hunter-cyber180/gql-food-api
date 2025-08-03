060ac35e065aae6b4d89912311df988aba6a22e7
unknown <a.shataie2023@gmail.com>
2025-08-02 22:57:13 -0700

    feat: Create add category resolver

--- graphql/resolvers/category.resolvers.js
+++ graphql/resolvers/category.resolvers.js
@@ -3,11 +3,18 @@ const CategoryModel = require("../../models/Category");
 
 categories = async () => await CategoryModel.find({});
 
+const addCategory = async (_, args, context) => {
+    const { title, icon } = args;
+    // TODO => validate admin token
+    return await CategoryModel.create({ title, icon });
+}
+
 const deleteCategory = async ({ id: _id }) => {
     return await CategoryModel.findOneAndDelete({ _id });
 }
 
 module.exports = {
     categories,
-    deleteCategory
+    deleteCategory,
+    addCategory
 }
