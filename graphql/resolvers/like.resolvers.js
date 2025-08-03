13c170008f7f8fa18f917d7f0c14a75976420d0c
unknown <a.shataie2023@gmail.com>
2025-08-02 23:09:01 -0700

    feat: Create add like resolver

--- graphql/resolvers/like.resolvers.js
+++ graphql/resolvers/like.resolvers.js
@@ -3,6 +3,13 @@ const LikeModel = require("../../models/Like");
 
 const likes = async () => await LikeModel.find({});
 
+const addLike = async (_, args, context) => {
+    const { user, food } = args;
+    // TODO => validate admin token
+    return await LikeModel.create({ user, food });
+}
+
 module.exports = {
-    likes
+    likes,
+    addLike
 };
