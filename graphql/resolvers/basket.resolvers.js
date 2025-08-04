8589f8d4e5400691169ce36e5b7ed57529a540ca
unknown <a.shataie2023@gmail.com>
2025-08-03 23:35:22 -0700

    feat: Create add basket resolver

--- graphql/resolvers/basket.resolvers.js
+++ graphql/resolvers/basket.resolvers.js
@@ -5,7 +5,14 @@ const baskets = async () => await BasketModel.find({});
 
 const basket = async ({ id: _id }) => await BasketModel.findOne({ _id });
 
+const addBasket = async (_, args, context) => {
+    const { quantity, user, food } = args;
+    // TODO => validate admin token
+    return await BasketModel.create({ quantity, user, food });
+}
+
 module.exports = {
     baskets,
-    basket
+    basket,
+    addBasket
 };
