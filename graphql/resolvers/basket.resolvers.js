0a164e8ad304b7a6dafc138490bfdfd0f00e184c
unknown <a.shataie2023@gmail.com>
2025-08-03 23:33:52 -0700

    feat: Create get basket resolver

--- graphql/resolvers/basket.resolvers.js
+++ graphql/resolvers/basket.resolvers.js
@@ -3,6 +3,9 @@ const BasketModel = require("../../models/Basket");
 
 const baskets = async () => await BasketModel.find({});
 
+const basket = async ({ id: _id }) => await BasketModel.findOne({ _id });
+
 module.exports = {
-    baskets
+    baskets,
+    basket
 };
