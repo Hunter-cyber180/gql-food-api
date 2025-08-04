1eb3d5a902fc192f37059b65f0dae7f863cc3185
unknown <a.shataie2023@gmail.com>
2025-08-03 23:42:07 -0700

    feat: Create get order resolver

--- graphql/resolvers/order.resolvers.js
+++ graphql/resolvers/order.resolvers.js
@@ -3,6 +3,9 @@ const OrderModel = require("../../models/Order");
 
 const orders = async () => await OrderModel.find({});
 
+const order = async ({ id: _id }) => await OrderModel.findOne({ _id });
+
 module.exports = {
     orders,
+    order
 };
