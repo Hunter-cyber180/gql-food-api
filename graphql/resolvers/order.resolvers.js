799e642aa94305b5021c6da3154e71103a2d3840
unknown <a.shataie2023@gmail.com>
2025-08-04 23:34:09 -0700

    feat: Create add order resolver

--- graphql/resolvers/order.resolvers.js
+++ graphql/resolvers/order.resolvers.js
@@ -5,7 +5,14 @@ const orders = async () => await OrderModel.find({});
 
 const order = async ({ id: _id }) => await OrderModel.findOne({ _id });
 
+const addOrder = async (_, args, context) => {
+    const { quantity, user, food, price, isDeliver } = args;
+    // TODO => validate admin token
+    return await OrderModel.create({ quantity, user, food, price, isDeliver });
+}
+
 module.exports = {
     orders,
-    order
+    order,
+    addOrder
 };
