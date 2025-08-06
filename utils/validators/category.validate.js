fde628dc083fbdfd51a8ed877436d16648d59bd9
unknown <a.shataie2023@gmail.com>
2025-08-05 23:54:10 -0700

    feat: Add icon field to category data validator

--- utils/validators/category.validate.js
+++ utils/validators/category.validate.js
@@ -1,10 +1,14 @@
 const Schema = require("validate");
-const mongoose = require("mongoose");
 
 const categorySchema = new Schema({
     title: {
         type: String,
         required: true,
         message: "Title is required!",
+    },
+    icon: {
+        type: String,
+        required: true,
+        message: "Icon is required!",
     },
 });
