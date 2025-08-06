bb688f717fe27c0708efcf712cebe052a6b61f67
unknown <a.shataie2023@gmail.com>
2025-08-05 23:49:41 -0700

    feat: Add userID field to order data validator

---
+++ utils/validators/order.validate.js
@@ -0,0 +1,13 @@
+const Schema = require("validate");
+const mongoose = require("mongoose");
+
+const orderSchema = new Schema({
+    user: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),
+            message: "userID must be a valid MongoDB ObjectId!"
+        }
+    }
+});
