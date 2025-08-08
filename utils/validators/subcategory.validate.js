0bd08dbbfa3894d9575f9ad3b555fef86b310f01
unknown <a.shataie2023@gmail.com>
2025-08-07 23:31:12 -0700

    feat: Add categoryID field to validate subCategory data

---
+++ utils/validators/subcategory.validate.js
@@ -0,0 +1,13 @@
+const Schema = require("validate");
+const mongoose = require("mongoose");
+
+const subCategorySchema = new Schema({
+    category: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),
+            message: "categoryID must be a valid MongoDB ObjectId!"
+        }
+    },
+});
