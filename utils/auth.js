e0c0493b6e1db072acdfce553150c32bb1a0d62d
unknown <a.shataie2023@gmail.com>
2025-08-08 22:43:37 -0700

    feat: Check user token in auth validator

--- utils/auth.js
+++ utils/auth.js
@@ -5,4 +5,8 @@ const authValidator = async (req) => {
     const authentication_header = req.headers.authorization;
     if (!authentication_header)
         throw new Error("Please login first!");
+
+    const token = authHeader.replace("Bearer ", "");
+    if (!token)
+        throw new Error("No token found!");
 };
