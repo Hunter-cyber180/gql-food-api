80b760fa55a762109393c6ba4628cbe28ffac278
unknown <a.shataie2023@gmail.com>
2025-08-09 23:40:30 -0700

    refactor: Add order does not require admin role

--- graphql/resolvers/order.resolvers.js
+++ graphql/resolvers/order.resolvers.js
@@ -7,7 +7,7 @@ const order = async ({ id: _id }) => await OrderModel.findOne({ _id });
 
 const addOrder = async (_, args, context) => {
     const { quantity, user, food, price, isDeliver } = args;
-    // TODO => validate admin token
+
     return await OrderModel.create({ quantity, user, food, price, isDeliver });
 }
 
