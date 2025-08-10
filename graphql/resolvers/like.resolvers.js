b85515b6d2e9a4fe330e404d3542547844645eb9
unknown <a.shataie2023@gmail.com>
2025-08-09 23:39:15 -0700

    refactor: Add like does not require admin role

--- graphql/resolvers/like.resolvers.js
+++ graphql/resolvers/like.resolvers.js
@@ -5,7 +5,6 @@ const likes = async () => await LikeModel.find({});
 
 const addLike = async (_, args, context) => {
     const { user, food } = args;
-    // TODO => validate admin token
     return await LikeModel.create({ user, food });
 }
 
