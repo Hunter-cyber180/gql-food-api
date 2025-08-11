929606ec51597e700852501f999fa79a9af99f74
unknown <a.shataie2023@gmail.com>
2025-08-10 23:38:25 -0700

    feat: Create get like resolver

--- graphql/resolvers/like.resolvers.js
+++ graphql/resolvers/like.resolvers.js
@@ -3,6 +3,9 @@ const LikeModel = require("../../models/Like");
 
 const likes = async () => await LikeModel.find({});
 
+const like = async ({ id: _id }) => await LikeModel.findOne({ _id });
+
+
 const addLike = async (_, args, context) => {
     const { user, food } = args;
     return await LikeModel.create({ user, food });
