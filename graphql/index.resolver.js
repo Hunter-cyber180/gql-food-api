36b209c55297967b82108fd29416dd7cf3ae45ef
unknown <a.shataie2023@gmail.com>
2025-08-11 23:34:25 -0700

    feat: Add subCategories query to root resolver

--- graphql/index.resolver.js
+++ graphql/index.resolver.js
@@ -10,6 +10,7 @@ const RootResolvers = {
     Query: {
         categories: categoryResolvers.categories,
         category: categoryResolvers.category,
+        subCategories: subCategoryResolvers.subCategories,
         foods: foodResolvers.foods,
         food: foodResolvers.food,
         likes: likeResolvers.likes,
