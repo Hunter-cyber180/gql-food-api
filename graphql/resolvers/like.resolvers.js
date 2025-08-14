0cda8bed52ccb89326c3fe6e656d05325db8bffd
unknown <a.shataie2023@gmail.com>
2025-08-13 23:05:51 -0700

    docs: Add comments to like resolvers

--- graphql/resolvers/like.resolvers.js
+++ graphql/resolvers/like.resolvers.js
@@ -1,17 +1,22 @@
 // * ---- Models ----
 const LikeModel = require("../../models/Like");
 
+// Get all likes
 const likes = async () => await LikeModel.find({});
 
+// Get a single like by ID
 const like = async ({ id: _id }) => await LikeModel.findOne({ _id });
 
+// Add a new like
 const addLike = async (_, args, context) => {
     const { user, food } = args;
     return await LikeModel.create({ user, food });
 }
 
+// Delete a like
 const deleteLike = async ({ id: _id }) => await LikeModel.findOneAndDelete({ _id });
 
+// Export all like-related resolver functions
 module.exports = {
     likes,
     like,
