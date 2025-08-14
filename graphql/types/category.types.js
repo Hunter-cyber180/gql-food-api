7f541228c15d77651d594bf93be71cda1e9ba401
unknown <a.shataie2023@gmail.com>
2025-08-13 23:12:43 -0700

    docs: Add comments to category types

--- graphql/types/category.types.js
+++ graphql/types/category.types.js
@@ -1,15 +1,17 @@
+// Defines the Category type for GraphQL schema
 exports.Category = `
   type Category {
-    _id: ID
-    title: String
-    icon: String
-    foods: [Food]
+    _id: ID               // Unique identifier
+    title: String         // Display name of the category
+    icon: String          // Icon representation (URL/classname)
+    foods: [Food]         // Array of related food items
   }
 `;
 
+// Defines input type for Category mutations
 exports.CategoryInput = `
   input CategoryInput {
-    title: String
-    icon: String
+    title: String         // Optional updated title
+    icon: String          // Optional updated icon
   }
 `;
