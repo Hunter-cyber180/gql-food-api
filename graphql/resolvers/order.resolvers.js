115bd9f14482a27a2b1bfbb6ef727fb80f7811ed
unknown <a.shataie2023@gmail.com>
2025-08-13 23:07:02 -0700

    docs: Add comments to order resolvers

--- graphql/resolvers/order.resolvers.js
+++ graphql/resolvers/order.resolvers.js
@@ -1,20 +1,30 @@
 // * ---- Models ----
 const OrderModel = require("../../models/Order");
 
+// Get all orders
 const orders = async () => await OrderModel.find({});
 
+// Get a single order by ID
 const order = async ({ id: _id }) => await OrderModel.findOne({ _id });
 
+// Create a new order
 const addOrder = async (_, args, context) => {
     const { quantity, user, food, price, isDeliver } = args;
-
-    return await OrderModel.create({ quantity, user, food, price, isDeliver });
+    return await OrderModel.create({ 
+        quantity, 
+        user, 
+        food, 
+        price, 
+        isDeliver 
+    });
 }
 
+// Delete an order
 const deleteOrder = async ({ id: _id }) => {
     return await OrderModel.findOneAndDelete({ _id });
 }
 
+// Export all order-related resolver functions
 module.exports = {
     orders,
     order,
