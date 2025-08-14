f21d09a2311b605ba5ab098b1272f153a934c267
unknown <a.shataie2023@gmail.com>
2025-08-13 23:12:01 -0700

    docs: Add comments to basket types

--- graphql/types/basket.types.js
+++ graphql/types/basket.types.js
@@ -1,16 +1,18 @@
+// Basket type definition - represents a shopping basket/item
 exports.Basket = `
   type Basket {
-    _id: ID
-    user: User
-    food: Food
-    quantity: Int
+    _id: ID                // Unique identifier
+    user: User             // User who owns the basket
+    food: Food             // Food item in basket  
+    quantity: Int          // Quantity of the food item
   }
 `;
 
+// Basket input type - for creating/updating basket items
 exports.BasketInput = `
   input BasketInput {
-    user: String!
-    food: String!
-    quantity: Int!
+    user: String!          // Required user ID reference
+    food: String!          // Required food ID reference
+    quantity: Int!         // Required quantity (must be positive)
   }
 `;
