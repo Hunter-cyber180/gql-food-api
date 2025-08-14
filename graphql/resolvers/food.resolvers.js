90bad4130270f9ac04b614597f210df9995209f4
unknown <a.shataie2023@gmail.com>
2025-08-13 23:04:35 -0700

    docs: Add comments to food resolvers

--- graphql/resolvers/food.resolvers.js
+++ graphql/resolvers/food.resolvers.js
@@ -4,32 +4,47 @@ const FoodModel = require("../../models/Food");
 // * ---- Utils ----
 const { adminValidator } = require("../../utils/auth");
 
+// Create a new food item
 const addFood = async (args, req) => {
     const { name, price, inventory, image, category, subCategory } = args;
-    await adminValidator(context.req);
-
-    return await FoodModel.create({ name, price, category, image, inventory });
+    await adminValidator(req); // Admin permission check
+    
+    return await FoodModel.create({ 
+        name, 
+        price, 
+        category, 
+        image, 
+        inventory 
+    });
 }
 
+// Update an existing food item
 const editFood = async ({ id: _id }, args, context) => {
     const { name, price, inventory, image, category, subCategory } = args;
-    await adminValidator(context.req);
-
-    return await FoodModel.findOneAndUpdate({ _id }, { name, price, category, image, inventory });
+    await adminValidator(context.req); // Admin permission check
+    
+    return await FoodModel.findOneAndUpdate(
+        { _id }, 
+        { name, price, category, image, inventory }
+    );
 }
 
+// Get all food items
 const foods = async () => {
     return await FoodModel.find({});
 }
 
+// Get a single food item by ID
 const food = async ({ id: _id }) => {
     return await FoodModel.findOne({ _id });
 }
 
+// Delete a food item
 const deleteFood = async ({ id: _id }) => {
     return await FoodModel.findOneAndDelete({ _id });
 }
 
+// Export all food-related resolver functions
 module.exports = {
     foods,
     food,
