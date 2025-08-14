5f0939c99f72a44017079d0c83f724681d7036d9
unknown <a.shataie2023@gmail.com>
2025-08-13 23:14:00 -0700

    docs: Add comments to like types

--- graphql/types/like.types.js
+++ graphql/types/like.types.js
@@ -1,14 +1,16 @@
+// Defines the Like type representing user-food favorites
 exports.Like = `
   type Like {
-    _id: ID
-    user: User
-    food: Food
+    _id: ID         // Unique identifier for the like record
+    user: User      // Reference to the user who liked
+    food: Food      // Reference to the liked food item
   }
 `;
 
+// Input type for creating likes (user-food relationships)
 exports.LikeInput = `
   input LikeInput {
-    user: ID
-    food: ID
+    user: ID        // ID of the user (required when creating)
+    food: ID        // ID of the food item (required when creating)
   }
 `;
