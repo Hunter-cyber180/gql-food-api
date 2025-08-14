c863441d607339d73433bccc2412f26d0aa6df35
unknown <a.shataie2023@gmail.com>
2025-08-13 23:01:46 -0700

    docs: Add comments to basket resolvers

--- graphql/resolvers/basket.resolvers.js
+++ graphql/resolvers/basket.resolvers.js
@@ -1,27 +1,28 @@
 // * ---- Models ----
 const BasketModel = require("../../models/Basket");
 
-// * ---- Utils ----
-const { adminValidator } = require("../../utils/auth");
-
+// Get all baskets
 const baskets = async () => await BasketModel.find({});
 
+// Get a single basket by ID
 const basket = async ({ id: _id }) => await BasketModel.findOne({ _id });
 
+// Add a new basket
 const addBasket = async (_, args, context) => {
     const { quantity, user, food } = args;
-
     return await BasketModel.create({ quantity, user, food });
 }
 
+// Update an existing basket
 const editBasket = async ({ id: _id }, args, context) => {
     const { quantity, user, food } = args;
-
     return await BasketModel.findOneAndUpdate({ _id }, { quantity, user, food });
 }
 
+// Delete a basket
 const deleteBasket = async ({ id: _id }) => await BasketModel.findOneAndDelete({ _id })
 
+// Export all basket-related resolver functions
 module.exports = {
     baskets,
     basket,
