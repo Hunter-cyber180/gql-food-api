e08e575614b0fce350c9fff97dfed60726468266
unknown <a.shataie2023@gmail.com>
2025-08-13 23:13:23 -0700

    docs: Add comments to food types

--- graphql/types/food.types.js
+++ graphql/types/food.types.js
@@ -1,22 +1,24 @@
+// GraphQL Type Definition for Food items
 exports.Food = `
   type Food {
-    _id: ID
-    name: String
-    price: Int
-    category: Category
-    subCategory: SubCategory
-    inventory: Int
-    image: String
+    _id: ID                     // Unique identifier
+    name: String                // Display name of the food item
+    price: Int                  // Price in base currency units
+    category: Category          // Main category association
+    subCategory: SubCategory    // Sub-category classification  
+    inventory: Int              // Available stock quantity
+    image: String               // URL/path to food image
   }
 `;
 
+// Input type for creating/updating Food items
 exports.FoodInput = `
   input FoodInput {
-    name: String
-    price: Int
-    inventory: Int
-    image: String
-    category: ID
-    subCategory: ID
+    name: String                // Food name (optional for updates)
+    price: Int                 // Price value (optional)
+    inventory: Int             // Stock quantity (optional)
+    image: String              // Image reference (optional)
+    category: ID               // Category ID reference (optional)
+    subCategory: ID            // SubCategory ID reference (optional)
   }
 `;
