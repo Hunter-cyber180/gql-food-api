d5b267a37e110b9ed9d489e0daf626506566e962
unknown <a.shataie2023@gmail.com>
2025-08-13 23:03:30 -0700

    docs: Add comments to category resolvers

--- graphql/resolvers/category.resolvers.js
+++ graphql/resolvers/category.resolvers.js
@@ -4,28 +4,32 @@ const CategoryModel = require("../../models/Category");
 // * ---- Utils ----
 const { adminValidator } = require("../../utils/auth");
 
+// Get all categories
 const categories = async () => await CategoryModel.find({});
 
+// Get a single category by ID
 const category = async ({ id: _id }) => await CategoryModel.findOne({ _id });
 
+// Add a new category
 const addCategory = async (_, args, context) => {
     const { title, icon } = args;
-    await adminValidator(context.req);
-
+    await adminValidator(context.req); // Admin permission check
     return await CategoryModel.create({ title, icon });
 }
 
+// Update an existing category
 const editCategory = async ({ id: _id }, args, context) => {
     const { title, icon } = args;
-    await adminValidator(context.req);
-    
+    await adminValidator(context.req); // Admin permission check
     return await CategoryModel.findOneAndUpdate({ _id }, { title, icon });
 }
 
+// Delete a category
 const deleteCategory = async ({ id: _id }) => {
     return await CategoryModel.findOneAndDelete({ _id });
 }
 
+// Export all category-related resolver functions
 module.exports = {
     categories,
     category,
