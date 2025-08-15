aea95d61a058c1fc3fb574f95061190ddfa3ac26
unknown <a.shataie2023@gmail.com>
2025-08-14 23:55:42 -0700

    docs: Add comments to like model

--- models/Like.js
+++ models/Like.js
@@ -1,9 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Like Schema Definition:
 const LikeSchema = new Schema({
-    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
-    food: { type: mongoose.Types.ObjectId, ref: "Food", required: true }
-}, { timestamps: true });
+    // Reference to the User who liked the food item
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",                        // References the User model
+        required: [true, 'User reference is required'],
+        index: true                        // Optimizes queries filtering by user
+    },
+    // Reference to the Food item that was liked
+    food: {
+        type: mongoose.Types.ObjectId,
+        ref: "Food",                       // References the Food model
+        required: [true, 'Food reference is required'],
+        index: true                       // Optimizes queries filtering by food
+    }
+}, {
+    // Schema Options:
+    timestamps: true,
+});
 
 module.exports = mongoose.model("Like", LikeSchema);
