9baf80b42f9c6a34cd85d3cdd67236ef0f1a5acd
unknown <a.shataie2023@gmail.com>
2025-08-14 23:49:54 -0700

    docs: Add comments to basket model

--- models/Basket.js
+++ models/Basket.js
@@ -1,10 +1,27 @@
+// Import required Mongoose modules
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// Define the Basket schema (structure for MongoDB documents)
 const BasketSchema = new Schema({
-    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
-    food: { type: mongoose.Types.ObjectId, ref: "Food", required: true },
-    quantity: { type: Number, required: true }
-}, { timestamps: true });
+    // Reference to the User who owns this basket item
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    // Reference to the Food item in the basket
+    food: {
+        type: mongoose.Types.ObjectId,
+        ref: "Food",
+        required: true
+    },
+    // Quantity of the food item in the basket
+    quantity: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });  // Adds automatic createdAt and updatedAt fields
 
+// Create and export the Basket model based on the schema
 module.exports = mongoose.model("Basket", BasketSchema);
