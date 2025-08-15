1e88c667046d208987836efac00b5438d5e4f328
unknown <a.shataie2023@gmail.com>
2025-08-14 23:37:45 -0700

    feat: Add comments to subCategory types

--- graphql/types/subcategory.types.js
+++ graphql/types/subcategory.types.js
@@ -1,18 +1,22 @@
+// GraphQL type definition for SubCategory
+// Defines the structure of a SubCategory object in the GraphQL schema
 exports.SubCategory = `
   type SubCategory {
-    _id: ID
-    category: Category
-    title: String
-    icon: String
-    foods: [Food]
+    _id: ID                     // Unique identifier for the subcategory
+    category: Category           // Reference to the parent Category
+    title: String               // Name/title of the subcategory
+    icon: String                // Icon representing the subcategory
+    foods: [Food]               // Array of Food items belonging to this subcategory
   }
 `;
 
+// GraphQL input type for SubCategory
+// Used for creating or updating SubCategory entries
 exports.SubCategoryInput = `
   input SubCategoryInput {
-    category: ID
-    title: String
-    icon: String
-    foods: [ID]
+    category: ID                // ID reference to the parent Category
+    title: String              // Name/title of the subcategory
+    icon: String               // Icon representing the subcategory
+    foods: [ID]                // Array of Food IDs belonging to this subcategory
   }
 `;
