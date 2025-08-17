3780e66792d89f24a88087477943f5463f04fec0
unknown <a.shataie2023@gmail.com>
2025-08-16 22:13:59 -0700

    docs: Add comments to category validator

--- utils/validators/category.validate.js
+++ utils/validators/category.validate.js
@@ -1,19 +1,20 @@
-const Schema = require("validate");
+const Schema = require("validate"); // Import the validation library
 
+// Define validation schema for category
 const categorySchema = new Schema({
     title: {
         type: String,
         required: true,
-        message: "Title is required!",
+        message: "Title is required!", // Error message if title is missing
     },
     icon: {
         type: String,
         required: true,
-        message: "Icon is required!",
+        message: "Icon is required!", // Error message if icon is missing
     },
 });
 
+// Export the validator function
 module.exports = {
-    categoryValidator: (object) => categorySchema.validate(object)
+    categoryValidator: (object) => categorySchema.validate(object) // Function to validate category objects
 };
-
