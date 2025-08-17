8b51526011f6353a80b952aa1fffa94123c92052
unknown <a.shataie2023@gmail.com>
2025-08-16 22:21:29 -0700

    docs: Add comments to user register validator

--- utils/validators/user.validate.js
+++ utils/validators/user.validate.js
@@ -3,26 +3,26 @@ const Schema = require("validate");
 const registerSchema = new Schema({
     name: {
         type: String,
-        required: true,
-        message: "Name is required!",
+        required: true,  // Name field is mandatory
+        message: "Name is required!",  // Error message if missing
     },
     email: {
         type: String,
-        required: true,
-        match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-        message: "Email is not valid!",
+        required: true,  // Email field is mandatory
+        match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,  // Basic email format validation
+        message: "Email is not valid!",  // Error message for invalid email
     },
     password: {
         type: String,
-        required: true,
-        match: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
-        message: "Password is not strong!",
+        required: true,  // Password field is mandatory
+        match: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,  // Strong password requirements
+        message: "Password is not strong!",  // Error message for weak password
     },
     phoneNumber: {
         type: String,
-        required: true,
-        match: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
-        message: "phoneNumber is not valid!",
+        required: true,  // Phone number field is mandatory
+        match: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,  // International phone number format
+        message: "phoneNumber is not valid!",  // Error message for invalid phone
     },
 });
 
