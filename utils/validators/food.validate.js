26fc286d94ed52e3f23cb59db5eeabda216a08a6
unknown <a.shataie2023@gmail.com>
2025-08-16 22:15:44 -0700

    docs: Add comments to food validator

--- utils/validators/food.validate.js
+++ utils/validators/food.validate.js
@@ -1,45 +1,47 @@
-const Schema = require("validate");
-const mongoose = require("mongoose");
+const Schema = require("validate");  // Import validation library
+const mongoose = require("mongoose"); // Import MongoDB library
 
+// Food item validation schema
 const foodSchema = new Schema({
     category: {
         type: String,
         required: true,
         validate: {
-            validator: (v) => mongoose.Types.ObjectId.isValid(v),
-            message: "categoryID must be a valid MongoDB ObjectId!"
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),  // Validate as MongoDB ObjectID
+            message: "categoryID must be a valid MongoDB ObjectId!" // Error message
         }
     },
     subCategory: {
         type: String,
         required: true,
         validate: {
-            validator: (v) => mongoose.Types.ObjectId.isValid(v),
-            message: "SubCategoryID must be a valid MongoDB ObjectId!"
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),  // Validate as MongoDB ObjectID
+            message: "SubCategoryID must be a valid MongoDB ObjectId!" // Error message
         }
     },
     name: {
         type: String,
         required: true,
-        message: "Name is required!",
+        message: "Name is required!",  // Error message if missing
     },
     image: {
         type: String,
         required: true,
-        message: "Image is required!",
+        message: "Image is required!",  // Error message if missing
     },
     inventory: {
         type: String,
         required: true,
-        message: "Inventory is required!",
+        message: "Inventory is required!",  // Error message if missing
     },
     price: {
         type: String,
         required: true,
-        message: "Price is required!",
+        message: "Price is required!",  // Error message if missing
     },
 });
 
+// Export validation function
 module.exports = {
-    foodValidator: (object) => foodSchema.validate(object)
+    foodValidator: (object) => foodSchema.validate(object)  // Validates food objects
 };
