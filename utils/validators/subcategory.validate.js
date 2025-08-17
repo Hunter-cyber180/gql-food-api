4329ea64656baa1ddf44f01f6d7c91338efe7a71
unknown <a.shataie2023@gmail.com>
2025-08-16 22:20:27 -0700

    docs: Add comments to subCategory validator

--- utils/validators/subcategory.validate.js
+++ utils/validators/subcategory.validate.js
@@ -1,27 +1,29 @@
-const Schema = require("validate");
-const mongoose = require("mongoose");
+const Schema = require("validate"); // Import validation library
+const mongoose = require("mongoose"); // Import MongoDB library
 
+// SubCategory validation schema
 const subCategorySchema = new Schema({
     category: {
         type: String,
-        required: true,
+        required: true,  // Parent category reference is required
         validate: {
-            validator: (v) => mongoose.Types.ObjectId.isValid(v),
-            message: "categoryID must be a valid MongoDB ObjectId!"
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),  // Validate as MongoDB ObjectID
+            message: "categoryID must be a valid MongoDB ObjectId!" // Error message
         }
     },
     title: {
         type: String,
-        required: true,
-        message: "Title is required!",
+        required: true,  // Subcategory name is required
+        message: "Title is required!",  // Error message if missing
     },
     icon: {
         type: String,
-        required: true,
-        message: "Icon is required!",
+        required: true,  // Icon reference is required
+        message: "Icon is required!",  // Error message if missing
     },
 });
 
+// Export validation function
 module.exports = {
-    subCategoryValidator: (object) => subCategorySchema.validate(object)
+    subCategoryValidator: (object) => subCategorySchema.validate(object)  // Validates subcategory objects
 };
