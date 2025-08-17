ad47a789864181b6d234421b39c61da63a57b846
unknown <a.shataie2023@gmail.com>
2025-08-16 22:17:25 -0700

    docs: Add comments to like validator

--- utils/validators/like.validate.js
+++ utils/validators/like.validate.js
@@ -1,25 +1,27 @@
-const Schema = require("validate");
-const mongoose = require("mongoose");
+const Schema = require("validate"); // Import validation library
+const mongoose = require("mongoose"); // Import MongoDB library
 
+// Like validation schema for tracking user-food likes
 const likeSchema = new Schema({
     user: {
         type: String,
-        required: true,
+        required: true,  // User reference is mandatory
         validate: {
-            validator: (v) => mongoose.Types.ObjectId.isValid(v),
-            message: "userID must be a valid MongoDB ObjectId!"
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),  // Validate MongoDB ObjectID format
+            message: "userID must be a valid MongoDB ObjectId!"   // Validation error message
         }
     },
     food: {
         type: String,
-        required: true,
+        required: true,  // Food reference is mandatory
         validate: {
-            validator: (v) => mongoose.Types.ObjectId.isValid(v),
-            message: "foodID must be a valid MongoDB ObjectId!"
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),  // Validate MongoDB ObjectID format
+            message: "foodID must be a valid MongoDB ObjectId!"    // Validation error message
         }
     },
 });
 
+// Export the validation function
 module.exports = {
-    likeValidator: (object) => likeSchema.validate(object)
+    likeValidator: (object) => likeSchema.validate(object)  // Function to validate like objects
 };
