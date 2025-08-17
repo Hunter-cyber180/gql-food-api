b716fa816091213645b2bc0545d5f418cb3076c6
unknown <a.shataie2023@gmail.com>
2025-08-16 22:12:32 -0700

    docs: Add comments to basket validator

--- utils/validators/basket.validate.js
+++ utils/validators/basket.validate.js
@@ -1,12 +1,13 @@
-const Schema = require("validate");
-const mongoose = require("mongoose");
+const Schema = require("validate"); // Import validate library
+const mongoose = require("mongoose"); // Import mongoose for MongoDB operations
 
+// Define basket schema for validation
 const basketSchema = new Schema({
     user: {
         type: String,
         required: true,
         validate: {
-            validator: (v) => mongoose.Types.ObjectId.isValid(v),
+            validator: (v) => mongoose.Types.ObjectId.isValid(v), // Validate as MongoDB ObjectId
             message: "userID must be a valid MongoDB ObjectId!"
         }
     },
@@ -14,17 +15,18 @@ const basketSchema = new Schema({
         type: String,
         required: true,
         validate: {
-            validator: (v) => mongoose.Types.ObjectId.isValid(v),
+            validator: (v) => mongoose.Types.ObjectId.isValid(v), // Validate as MongoDB ObjectId
             message: "foodID must be a valid MongoDB ObjectId!"
         }
     },
     quantity: {
-        type: String,
+        type: String, // Note: Consider using Number if quantity should be numeric
         required: true,
         message: "Quantity is required!",
     },
 });
 
+// Export validation function
 module.exports = {
     basketValidator: (object) => basketSchema.validate(object)
 };
