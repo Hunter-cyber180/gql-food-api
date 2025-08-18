36d5c9ae462fae928cdd63dccc11a81d6b63d542
unknown <a.shataie2023@gmail.com>
2025-08-17 23:47:32 -0700

    feat: Add food relation to order in root resolver

--- graphql/index.resolver.js
+++ graphql/index.resolver.js
@@ -67,12 +67,15 @@ const RootResolvers = {
         },
         food: async (parent) => {
             return FoodModel.find({ _id: parent.food });
-        }
+        },
     },
     Order: {
         user: async (parent) => {
             return UserModel.find({ _id: parent.user });
         },
+        food: async (parent) => {
+            return FoodModel.find({ _id: parent.food });
+        }
     }
 };
 
